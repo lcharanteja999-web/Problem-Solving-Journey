e199a14663105de375548ceddc8167fc659f4a93
Lakkakula.Charan Teja <l.charanteja999@gmail.com>
2026-06-24 14:43:04 +0530

    Update first_negative_number.py

--- DSA/Arrays/Sliding_windows.py/first_negative_number.py
+++ DSA/Arrays/Sliding_windows.py/first_negative_number.py
@@ -22,4 +22,36 @@ for i in range(len(arr) - k + 1):
 
 print(l)
 
+#-----------------liding window-------------------
+# Time Complexity: O(n)
+# Space Complexity: O(k)
+
+from collections import deque
+
+arr = [12, -1, -7, 8, -15, 30, 16, 28]
+k = 3
+
+q = deque()
+ans = []
+
+for i in range(len(arr)):
+
+    # Add negative index
+    if arr[i] < 0:
+        q.append(i)
+
+    # Remove indexes outside current window
+    if q and q[0] <= i - k:
+        q.popleft()
+
+    # First complete window formed
+    if i >= k - 1:
+        if q:
+            ans.append(arr[q[0]])
+        else:
+            ans.append(0)
+
+print(ans)
+
+
 
