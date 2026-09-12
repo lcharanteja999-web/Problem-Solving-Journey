8a213d341fd25df6776e1c939e7810e90b442318
Lakkakula.Charan Teja <l.charanteja999@gmail.com>
2026-09-12 21:37:28 +0530

    Create Matrix-Multiplication.cpp

---
+++ C++_practice-problems/Matrix-Multiplication.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[2][2];
+    int b[2][2];
+    int c[2][2];
+
+    cout << "Enter elements of matrix A:\n";
+
+    for(int i = 0; i < 2; i++)
+    {
+        for(int j = 0; j < 2; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    cout << "Enter elements of matrix B:\n";
+
+    for(int i = 0; i < 2; i++)
+    {
+        for(int j = 0; j < 2; j++)
+        {
+            cin >> b[i][j];
+        }
+    }
+
+    for(int i = 0; i < 2; i++)
+    {
+        for(int j = 0; j < 2; j++)
+        {
+            c[i][j] = 0;
+
+            for(int k = 0; k < 2; k++)
+            {
+                c[i][j] = c[i][j] + a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    cout << "Matrix multiplication is:\n";
+
+    for(int i = 0; i < 2; i++)
+    {
+        for(int j = 0; j < 2; j++)
+        {
+            cout << c[i][j] << " ";
+        }
+
+        cout << "\n";
+    }
+}
